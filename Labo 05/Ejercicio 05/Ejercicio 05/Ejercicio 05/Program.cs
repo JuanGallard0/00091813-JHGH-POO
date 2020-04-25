ae5896da777c1c5061ea1c78a02b7a2fd35f880f
Juan Gallardo <00091813@uca.edu.sv>
2020-04-24 21:23:35 -0600

    Add: ex 05 lab 05

---
+++ Labo 05/Ejercicio 05/Ejercicio 05/Ejercicio 05/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Ejercicio_05
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("Ingrese frase: ");
+            String frase = Console.ReadLine();
+            frase = frase.ToLower();
+            List<char> vocalesFrase = new List<char>();
+            for (int i = 0; i < frase.Length; i++)
+            {
+                if (frase[i] == 'a' || frase[i] == 'e' || 
+                    frase[i] == 'i' || frase[i] == 'o' || frase[i] == 'u')
+                    vocalesFrase.Add(frase[i]);
+            }
+
+            foreach (char vocal in vocalesFrase)
+                Console.Write(vocal + ", ");
+        }
+    }
+}
