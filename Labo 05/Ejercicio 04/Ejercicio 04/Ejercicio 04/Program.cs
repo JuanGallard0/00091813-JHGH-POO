fc90b406d93864e90702ec144bcbdcd9958c6b8c
Juan Gallardo <00091813@uca.edu.sv>
2020-04-24 21:14:41 -0600

    Add: ex 04 lab 05

---
+++ Labo 05/Ejercicio 04/Ejercicio 04/Ejercicio 04/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Ejercicio_04
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("Ingrese frase: ");
+            String frase = Console.ReadLine();
+            frase = frase.ToLower();
+            Console.WriteLine("Frase modificada: " + frase.Replace('a', 'b'));
+        }
+    }
+}
