0251f258ede34b4573c4993fc7d85ed542184e2e
Juan Gallardo <00091813@uca.edu.sv>
2020-04-24 20:36:10 -0600

    Add: ex 01 lab 05

---
+++ Labo 05/Ejercicio 01/Ejercicio 01/Ejercicio 01/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Ejercicio_01
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("Ingrese n:");
+            int n = Convert.ToInt16(Console.ReadLine());
+            int[] fibonacciMemory = new int[n];
+            Console.WriteLine("Primeros " + n + " terminos de fibonacci:");
+            fibonacciMemory[0] = 0;
+            fibonacciMemory[1] = 1;
+            for (int i = 0; i < n; i++)
+            {
+                if (i > 1)
+                    fibonacciMemory[i] = fibonacciMemory[i - 1] + fibonacciMemory[i - 2];
+                Console.Write(fibonacciMemory[i] + ", ");
+            }
+        }
+    }
+}
