c9fc3ab36a5aee63529233c318b2a7ac27fea183
Juan Gallardo <00091813@uca.edu.sv>
2020-04-24 23:01:32 -0600

    Add: meet2304

---
+++ Meet/Meet2304/Meet2304/Meet2304/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Meet2304
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var p = new List<Persona>();
+            
+            p.Add(new Persona("Juan","Gallardo",24 ));
+            p.Add(new Persona("Marco",edad:19 ));
+            p.Add(new Persona(edad: 60 ));
+            p.Add(new Persona(apellido:"Hernandez" ));
+
+            String buscar = "Juan";
+            buscar = Console.ReadLine();
+            
+            p.ForEach(it =>
+            {
+                if (buscar.Equals(it.Nombre))
+                {
+                    Console.WriteLine("Nuevo nombre ");
+                    it.Nombre = Console.ReadLine();
+                    Console.WriteLine(it.ToString());
+                }
+            });
+            int añadir = 12;
+            int árbol = 14;
+        }
+    }
+}
