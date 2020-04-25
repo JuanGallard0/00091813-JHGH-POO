1286225cc53726d5db04a2fdfd408e8d01a92a09
Juan Gallardo <00091813@uca.edu.sv>
2020-04-24 21:08:40 -0600

    Add: ex 03 lab 05

---
+++ Labo 05/Ejercicio 03/Ejercicio 03/Ejercicio 03/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Ejercicio_03
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("Ingrese n: ");
+            int n = Convert.ToInt16(Console.ReadLine());
+            Console.WriteLine("Secuencia: ");
+            secuencia(n, 1);
+        }
+
+        public static void secuencia(int n, int i)
+        {
+            if (i < n)
+            {
+                Console.Write(i + ", ");
+                secuencia(n, i + 1);
+            }
+            Console.Write(i + ", ");
+        }
+    }
+}
