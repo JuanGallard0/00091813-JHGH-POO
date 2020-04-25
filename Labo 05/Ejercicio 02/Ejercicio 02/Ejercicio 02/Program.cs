353c9626312faa507a01448f85eaf48ecac51151
Juan Gallardo <00091813@uca.edu.sv>
2020-04-24 21:02:54 -0600

    Add: ex 02 lab 05

---
+++ Labo 05/Ejercicio 02/Ejercicio 02/Ejercicio 02/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Ejercicio_02
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("Ingrese una frase: ");
+            String frase = Console.ReadLine();
+            frase = frase.ToLower();
+            String fraseInvertida = "";
+            for (int i = frase.Length; i > 0; i--)
+                fraseInvertida += frase[i - 1]; 
+            if (frase.CompareTo(fraseInvertida) == 0)
+                Console.WriteLine("La frase es palindrome.");
+            else 
+                Console.WriteLine("La frase no es palindrome.");
+        }
+    }
+}
